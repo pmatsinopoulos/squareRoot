0d9189b828e278323fb1a382da6b459b9d5ef872
Panayotis Matsinopoulos <Panayotis Matsinopoulos panayotis@matsinopoulos.gr>
2017-11-11 11:50:34 +0200

    Initial commit

---
+++ main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(void) {
+	float number = 0.0;
+
+	printf("Give me a non-negative number and I will find it's square root: ");
+	scanf("%f", &number);
+	printf("You gave: %f\n", number);
+	if (number < 0) {
+		printf("I cannot find the square root of a negative number\n");
+	}
+	else {
+		printf("The square root is: %f\n", sqrt(number));
+	}
+
+
+	int i, j, k;
+    k =
+	printf("\n%d\n", k);
+	return 0;
+}
